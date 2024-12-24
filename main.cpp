b621b968b2f23b87ea7ec3f10cb82a5679e0a26f
lmcosta10 <letmc14@gmail.com>
2024-12-24 17:37:31 +0000

    implement variable board

--- main.cpp
+++ main.cpp
@@ -2,5 +2,16 @@
 #include <vector>
 
 int main() {
-	std::vector<int> board;
+	int board_len;
+	std::cout << "Enter board length: ";
+	std::cin >> board_len;
+	
+	int board[board_len*board_len];
+	
+	for (int i = 0; i < board_len*board_len; i++) {
+		board[i] = rand() % 2;
+		std::cout << board[i];
+	}
+		
+	return 0;
 }
