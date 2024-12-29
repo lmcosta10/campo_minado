380f37d6d9775354c67ce021ad369748d7e5292f
lmcosta10 <letmc14@gmail.com>
2024-12-29 06:16:03 +0000

    check for mouse click

--- main.cpp
+++ main.cpp
@@ -2,10 +2,37 @@
 #include <vector>
 #include <GLFW/glfw3.h>
 
+struct Button {
+        double x0;
+        double x1;
+        double y0;
+        double y1;
+        int type; // TODO
+};
+
+
+// TODO
+int check_board_click () {
+        return 0;
+}
+
+
+void mouse_button_callback(GLFWwindow* window, int button, int action, int mods)
+{
+        double xpos;
+        double ypos;
+        glfwGetCursorPos(window, &xpos, &ypos);
+        if (button == GLFW_MOUSE_BUTTON_LEFT && action == GLFW_PRESS)
+                std::cout << "Click on x = " << xpos << "\n";
+}
+
+
 int main() {
 	int board_len;
 	std::cout << "Enter board length: ";
 	std::cin >> board_len;
+	
+	Button board_buttons[board_len*board_len];
 	
 	GLFWwindow* window;
 
@@ -26,13 +53,27 @@ int main() {
 		board[i] = rand() % 2;
 	}
 	
-	float delta = 0.02f;
+	float delta = 0.01f;
 	float square_len = 0.1f;
 	float start_x = 1.0/2.0 * square_len * board_len;
 	float start_y = 1.0/2.0 * square_len * board_len;
 	
+	/* Place buttons on board */
+	for (int i = 0; i < board_len; i++) {
+                for (int j = 0; j < board_len; j++) {
+                        board_buttons[board_len*i + j].x0 = 0.1f*j + delta - start_x;
+                        board_buttons[board_len*i + j].x1 = 0.1f*j + square_len - delta - start_x;
+                        board_buttons[board_len*i + j].y0 = 0.1f*i + delta - start_y;
+                        board_buttons[board_len*i + j].y1 = 0.1f*i + square_len - delta - start_y;
+                        board_buttons[board_len*i + j].type = 1;
+                }
+        }
+	
 	/* Make the window's context current */
         glfwMakeContextCurrent(window);
+        
+        /* Mouse click callback */
+        glfwSetMouseButtonCallback(window, mouse_button_callback);
 
         /* Loop until the user closes the window */
         while (!glfwWindowShouldClose(window))
@@ -40,16 +81,20 @@ int main() {
                 /* Render here */
                 glClear(GL_COLOR_BUFFER_BIT);
                 
+                // TODO: optimize loop (with the previous "for" one)
                 for (int i = 0; i < board_len; i++) {
                         for (int j = 0; j < board_len; j++) {
 		                glBegin(GL_QUADS);
-                                glVertex2f( 0.1f*j + delta - start_x, 0.1f*i + delta - start_y);
-                                glVertex2f( 0.1f*j + square_len - delta - start_x, 0.1f*i + delta - start_y);
-                                glVertex2f( 0.1f*j + square_len - delta - start_x, 0.1f*i + square_len - delta - start_y);
-                                glVertex2f( 0.1f*j + delta - start_x, 0.1f*i + square_len - delta - start_y);
+                                glVertex2f( board_buttons[board_len*i + j].x0, board_buttons[board_len*i + j].y0);
+                                glVertex2f( board_buttons[board_len*i + j].x1, board_buttons[board_len*i + j].y0);
+                                glVertex2f( board_buttons[board_len*i + j].x1, board_buttons[board_len*i + j].y1);
+                                glVertex2f( board_buttons[board_len*i + j].x0, board_buttons[board_len*i + j].y1);
                                 glEnd();
                         }
 	        }
+	        
+	        /* Testing */
+	        std::cout << "Test\n"; // TODO: understand
 
                 /* Swap front and back buffers */
                 glfwSwapBuffers(window);
